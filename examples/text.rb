fed9d88be5929f5b75fe893c506e2173c46945ba
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2017-04-13 14:43:30 +0700

    draw_text uses baseline, so adjust for that with box position

--- examples/text.rb
+++ examples/text.rb
@@ -10,9 +10,9 @@ Prawn::Document.generate "text_field.pdf" do |pdf|
   pdf.text("Forms Test", :size => 20)
 
   pdf.draw_text('First Name:', :at => [30, 600], :size => 12)
-  pdf.text_field("fname", 100, 600, 200, 16)
+  pdf.text_field("fname", 100, 600+12, 200, 16)
 
   pdf.draw_text("Last Name:", :at => [30, 560], :size => 12)
-  pdf.text_field("lname", 100, 560, 200, 16)
+  pdf.text_field("lname", 100, 560+12, 200, 16)
 
 end
